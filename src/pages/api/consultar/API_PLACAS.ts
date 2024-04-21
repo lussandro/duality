8f434a10febd0af2a5aec030c637cb849e37b3b8
lussandro.ilha <lusssandro.ilha@executiva.com.br>
2024-04-21 14:05:52 -0300

    Ajustes de modo geral em todo o sistem (mexi pra caralho)

--- src/pages/api/consultar/API_PLACAS.ts
+++ src/pages/api/consultar/API_PLACAS.ts
@@ -1,2 +1,2 @@
 // const API_BASE_URL_SERASA = `https://f5search.com.br/search/serasa?access-key=${process.env.API_SERASA}&cpf=`;
-const API_PLACAS = `https://wdapi2.com.br/consulta/${input}/${process.env.API_TOKEN}`;
+const API_PLACAS = `https://wdapi2.com.br/consulta`;
